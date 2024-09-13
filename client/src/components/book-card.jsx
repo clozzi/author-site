e0f2eed413455cb9702a511384feba2cc55bdc43
Chris Lozzi <clozzi12@gmail.com>
2024-09-13 16:33:21 -0400

    + book-card / added to shop | filler for about

---
+++ client/src/components/book-card.jsx
@@ -0,0 +1,12 @@
+function BookCard() {
+    return (
+        <div className="book-card">
+            <img src="" alt="" onClick={console.log('book clicked')}/>
+            <h3>Title of Book Here</h3>
+            <p>A short summary of the book here -- leave the wordsmithing to the professionals</p>
+            <a href="" />
+        </div>
+    )
+}
+
+export default BookCard
--- client/src/routes/shop.jsx
+++ client/src/routes/shop.jsx
@@ -1,8 +1,17 @@
+import BookCard from '../components/book-card';
+
 function Shop() {
     return (
         <div>
             <h1>Shop</h1>
             <p>Buy things here</p>
+            <div className='book-shop'>
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+            </div>
         </div>
     )
 }
