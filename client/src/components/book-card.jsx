4e150b375d4dc8965d4138c67911817453bcc883
Chris Lozzi <clozzi12@gmail.com>
2024-09-13 17:02:18 -0400

    fixed onclick placeholder

--- client/src/components/book-card.jsx
+++ client/src/components/book-card.jsx
@@ -1,10 +1,10 @@
 function BookCard() {
     return (
         <div className="book-card">
-            <img src="" alt="" onClick={console.log('book clicked')}/>
+            <img src="" alt="" onClick={() => console.log('book clicked')}/>
             <h3>Title of Book Here</h3>
             <p>A short summary of the book here -- leave the wordsmithing to the professionals</p>
-            <a href="" />
+            <button onClick={() => console.log('shop btn clicked')}>Learn More</button>
         </div>
     )
 }
