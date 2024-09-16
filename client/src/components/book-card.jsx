e67b835f898adadd9b8510b78e2896a222c0e6a2
Chris Lozzi <clozzi12@gmail.com>
2024-09-16 11:53:22 -0400

    messing around with db.json display

--- client/src/components/book-card.jsx
+++ client/src/components/book-card.jsx
@@ -1,8 +1,11 @@
-function BookCard() {
+function BookCard({ book }) {
+    console.log(book)
     return (
         <div className="book-card">
-            <img src="" alt="" onClick={() => console.log('book clicked')}/>
-            <h3>Title of Book Here</h3>
+            {/* <img src="" alt="" onClick={() => console.log('book clicked')}/> */}
+            <img src={book.image} alt="book cover" />
+            {/* <h3>Title of Book Here</h3> */}
+            <h3>{book.title}</h3>
             <p>A short summary of the book here -- leave the wordsmithing to the professionals</p>
             <button onClick={() => console.log('shop btn clicked')}>Learn More</button>
         </div>
--- client/src/routes/shop.jsx
+++ client/src/routes/shop.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from 'react';
 import BookCard from '../components/book-card';
 
 function Shop() {
+    const [books, setBooks] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:3000/books')
+        .then(r => r.json())
+        .then(books => {setBooks(books), console.log(books)})
+    }, [])
+
     return (
         <div>
             <h1>Shop</h1>
             <p>Buy things here</p>
             <div className='book-shop'>
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
+                {books ? (
+                    <div>
+                        {books.map(book => (
+                            <BookCard key={book.id} book={book} />
+                        ))}
+                    </div>
+                ) : (
+                    <div>
+                        <h2>Loading...</h2>
+                        <p>Worth the wait!</p>
+                    </div>
+                )}
             </div>
         </div>
     )
