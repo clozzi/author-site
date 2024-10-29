66a907f1cbc41b587e8b9d13eba06f6347ef8662
Chris Lozzi <clozzi12@gmail.com>
2024-10-29 11:34:51 -0400

    removed old imports

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,12 +1,5 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
 import author from '../images/author.jpg';
-import book1 from '../images/book.jpg';
-import book2 from '../images/book2.webp';
-import book3 from '../images/book3.jpg';
-import book4 from '../images/book4.jpg';
-import book5 from '../images/book5.jpg';
-import tales from '../images/tales_of_the_bacchae.jpg';
 import BookCarousel from '../components/book-carousel';
 
 function Index() {
